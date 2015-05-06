8eafb969a1264f30982d9328c0337c738aed7540
Earl <earl@digitalocean.com>
2015-05-06 15:10:28 -0400

    few changes

--- js/app.js
+++ js/app.js
@@ -14,9 +14,42 @@ $('.code-pre.command').each(function(){
 
 
 
- $('.layout-wrapper, .footer, .outside_viewport, .mobile-nav, header').remove();
+ // $('.layout-wrapper, .footer, .outside_viewport, .mobile-nav, header').remove();
 
 
 $('#code').html(string).text();
 
+
+var form = $(".userData"),
+	name = $(".real_name"),
+	email = $(".real_email"),
+	git = $(".real_git");
+
+	//gitURL = git.match(/url ={([^}]*)}/,"");
+
+form.submit(function(e){
+	e.preventDefault();
+
+	console.log(git[0].value);
+
+	var yourName = name.val(),
+	yourEmail = email.val();
+    
+    $(".git_real_name").each(function(){
+		$(this).text(yourName)
+	});
+
+	$(".git_email").each(function(){
+		$(this).text(yourEmail)
+	});
+
+	$(".git_version").each(function(){
+		$(this).text(git[0].value)
+	});
+
+	$(".validation").html(yourName + ", the tutorial has been customized based on the information you entered. <a class='bash' href='#'>Generate Bash Script</a>").addClass('open');
 });
+
+       
+});
+
