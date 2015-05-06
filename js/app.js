d7388b4ec959278c60a03899140a1c53657ceebe
Earl <earl@digitalocean.com>
2015-05-06 13:13:52 -0400

    BOOOOM MAKING THINGS

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
+$(function(){
 
-$('#base').load('https://www.digitalocean.com/community/tutorials/how-to-deploy-a-rails-app-with-unicorn-and-nginx-on-ubuntu-14-04');
+var string;
+
+
+$('body').append('<code id="code">');
+
+
+
+$('code').each(function(){
+	var stringAdd = $(this).html();
+	string += stringAdd;
+	string += '\n';
+	$('#code').html($('#code').html() + stringAdd + '\n');
+})
+
+
+
+ $('.layout-wrapper, .footer, .outside_viewport, .mobile-nav, header').remove();
+
+
+$('#code').html(string).text();
+
+});
